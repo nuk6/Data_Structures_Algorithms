ae77302136c45e14e23c0929e5b73867134b48a2
Nidhi <nidhi.kumari.met14@iitbhu.ac.in>
2019-01-24 20:21:33 +0530

    Update max_j-i_such_that_aj_is_greater_than_ai.cpp

--- max_j-i_such_that_aj_is_greater_than_ai.cpp
+++ max_j-i_such_that_aj_is_greater_than_ai.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
 using namespace std;
-typedef int ll;
-ll t,n;
+int t,n;
 int main() {
 	cin >> t;
 	while(t--)
 	{
 	    cin >> n;
-	    ll a[n],r[n];
-	    for(ll i = 0; i < n; ++i) cin >> a[i]; r[n-1] = a[n-1];
-	    for(ll i = n-2; i >=0; --i) r[i] = max(r[i+1],a[i]);
+	    int a[n],r[n];
+	    for(int i = 0; i < n; ++i) cin >> a[i]; r[n-1] = a[n-1];
+	    for(int i = n-2; i >=0; --i) r[i] = max(r[i+1],a[i]);
 	    int i = 0, j= 0, ans = -1;
 	    while(i < n && j < n)
 	    {
