a8fcc41c7b72ea0e2aad47e9afaf28c9cc709d54
Nidhi <nidhi.kumari.met14@iitbhu.ac.in>
2022-06-06 20:33:22 +0530

    Create binary_indexed_tree.cpp

---
+++ binary_indexed_tree.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MX = 20007;
+int bit[MX];
+
+void add(int pos, int v, int n) {
+	for(int i = pos+1; i <= n; i += (i&-i)) bit[i] += v;
+}
+
+void build(cvi a) {
+	int n = a.size();
+	for(int i = 0; i < n; ++i) {
+		add(i, a[i], n);
+	}
+}
+
+int query(int x, int y) {
+	int val1= 0, val2 = 0;
+	for(int i = x; i; i -= (i&-i)) {
+		val1 += bit[i];
+	} 
+	for(int i = y; i; i -= (i&-i)) {
+		val2 += bit[i];
+	} 
+	return val2 - val1;
+}
+
+int main() {
+	vector<int> a{1,2,-1,5,5};
+	build(a);
+	cout << query(2,4);
+}
